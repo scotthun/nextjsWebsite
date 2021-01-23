5ea255c343163923d1b1b6a23a50b84895c350e2
scotthun <scotthung1@gmail.com>
2021-01-22 18:45:17 -0800

    Add intro formats for mobile and desktop

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Layout from '../resources/components/MyLayout.js'
 import MyForm from '../resources/components/contact_form.js'
-import * as textGenerator from '../resources/components/text/intro.js' 
+import * as textGenerator from '../resources/functions/intro.js' 
 
 export default function Homepage() {
     return (
@@ -8,18 +8,18 @@ export default function Homepage() {
             {
               //block for desktop
             }
-            <div className="block" id= "intro">
+            <div className="block" id= "introDesktop">
               <div className="innerBlock">
-                <table class="firstTable" >
+                <table class="introTable" >
                 <tr>
                     <td>
                         
-                        <div id="bio">
+                        <div className="tdDiv">
                             <div className="descriptionText">
                             <p>
-                              <h3 id="hello"> Hi my name is</h3>
-                              <h1 id="nombre">Scott Hung.</h1>
-                              <h1 id="software">I love to write code.</h1>
+                              <h3 className="hello"> Hi my name is</h3>
+                              <h1 className="nombre">Scott Hung.</h1>
+                              <h1 className="software">I love to write code.</h1>
                               {textGenerator.getIntroText()}
                               {textGenerator.getSummaryText()}
                             </p>
@@ -27,7 +27,7 @@ export default function Homepage() {
                         </div>
                     </td>
                     <td>
-                      <img className="TextWrap" src="/images/headshot.PNG" />
+                      <img className="tablePicture" src="/images/headshot.PNG" />
                     </td>
                 </tr>
                 </table>
@@ -35,7 +35,17 @@ export default function Homepage() {
                
                 <p class="clear">&nbsp;</p>
             </div>
-            
+
+            <div className="block" id= "introMobile">
+              <div className="innerBlock">
+                <h3 className="hello"> Hi my name is</h3>
+                <h1 className="nombre">Scott Hung.</h1>
+                <h1 className="software">I love to write code.</h1>
+                <img id="mobileHeadshot" src="/images/headshot.PNG" />
+                {textGenerator.getIntroText()}
+                {textGenerator.getSummaryText()}
+              </div>
+            </div>
             <div className="block" id="experience">
               <div className="innerBlock">
                 <h2>2. Work Experience</h2>
@@ -207,31 +217,30 @@ export default function Homepage() {
                 {
                     `
                 
-                    #intro{
+                    #introDesktop{
                         background-color: #ffffff;
                         margin-top: 50px;
                         margin-bottom: 50px;
                     }
-                    td {
-                      font-size: 16px;
-                    }
                     
-                    .firstTable{
+                    .introTable{
                       width: 80%;
                     }
 
-                    .TextWrap {
+                    .tablePicture {
                       float:right;
                       width: 25em;
                       display: inline-block;
                       padding: 0;
                       vertical-align: middle;
                     }
-                  #bio
-                  {
-                    position:relative;
-                    top:0px;
-                  }
+
+                    .tdDiv
+                    {
+                      position:relative;
+                      top:0px;
+                    }
+
                     .clear{
                         clear: both;
                         margin-top: 0px;
@@ -242,20 +251,20 @@ export default function Homepage() {
                         margin: 0;
                     }
 
-                    #hello{
+                    .hello{
                         margin-bottom: 0px;
                         font-size: 14px;
                         font-family: 'Roboto Mono', monospace;
                     }
                     
-                    #nombre{
+                    .nombre{
                         font-size: 70px;
                         margin-top: 0px;
                         padding-top: 0px;
                         margin-bottom: 0px;
                     }
                     
-                    #software{
+                    .software{
                         font-size: 70px;
                         margin-top: 0px;
                         padding-top: 0px;
@@ -374,8 +383,12 @@ export default function Homepage() {
                         background-color: #ffffff;
 
                     }
+
+                    #introMobile{
+                      display: none;
+                    }
                 
-                    @media (min-width: 481px) and (max-width: 768px){
+                    @media (min-width: 481px) and (max-width: 751px){
 
                         #skillsLeft{
                             columns: 2;
@@ -390,9 +403,43 @@ export default function Homepage() {
                           height: 90%;
                           width: 99%;
                         }
+
+                        #introMobile{
+                          background-color: #ffffff;
+                          margin-top: 50px;
+                          margin-bottom: 50px;
+                          display: block;
+                        }
+
+                        #mobileHeadshot{
+                          width: 100%;
+                          margin-bottom: 1em;
+                        }
+
+                        #introDesktop{
+                          display: none;
+                          width: 100%;
+                        }
+
+                        
                     }
 
                     @media (max-width:480px){
+
+                        #introMobile{
+                          background-color: #ffffff;
+                          margin-top: 50px;
+                          margin-bottom: 50px;
+                          display: block;
+                        }
+
+                        #mobileHeadshot{
+                          width: 100%;
+                        }
+
+                        #introDesktop{
+                          display: none;
+                        }
 
                         #nombre{
                             font-size: 50px;
